3e9e4617dc5357bb81bcd66cefb9a511fae80df9
Christian Garbs <mitch@cgarbs.de>
2017-05-29 22:59:33 +0200

    BucketList: refine by Partitioner
    also refactor existing refinement to use Classifier interface for
    clarification
    
    Protip: Avoid type erasure by naming your types!

--- src/main/java/de/cgarbs/jadupes/filter/BucketList.java
+++ src/main/java/de/cgarbs/jadupes/filter/BucketList.java
@@ -23,6 +23,35 @@ import java.util.stream.Collectors;
  */
 public class BucketList<T>
 {
+	// TODO: check if further inner classes rise readability (eg. Bucket class)
+
+	/**
+	 * Identifies sub-buckets by a value for every element.
+	 * Elements mapping to the same value are put in the same sub-bucket.
+	 * 
+	 * @author Christian Garbs &lt;mitch@cgarbs.de&gt;
+	 *
+	 * @param <T>
+	 *            the type of elements in the bucket list
+	 * @param <R>
+	 *            the type of identifier created
+	 */
+	public interface Classifier<T, R> extends Function<T, R>
+	{
+	};
+
+	/**
+	 * Directly creates sub-buckets by distributing the elements of a bucket.
+	 * 
+	 * @author Christian Garbs &lt;mitch@cgarbs.de&gt;
+	 *
+	 * @param <T>
+	 *            the type of elements in the bucket list
+	 */
+	public interface Partitioner<T> extends Function<List<T>, Collection<List<T>>>
+	{
+	};
+
 	private final Collection<List<T>> buckets;
 
 	private BucketList(Collection<List<T>> buckets)
@@ -62,11 +91,28 @@ public class BucketList<T>
 	 *            extracts the new sub-bucket identifier for every element
 	 * @return a new bucket list
 	 */
-	public <R> BucketList<T> refine(Function<T, R> classifier)
+	public <R> BucketList<T> refine(Classifier<T, R> classifier)
 	{
 		return new BucketList<T>( //
 				buckets.stream() //
 						.flatMap(bucket -> partition(bucket, classifier).stream()) //
+						.collect(Collectors.toList()));
+	}
+
+	/**
+	 * Creates a new bucket list in which each existing bucket is split into
+	 * further sub-buckets.
+	 * Elements of different buckets are not mixed in the new sub-buckets.
+	 * 
+	 * @param partitioner
+	 *            creates the sub-lists for a given list
+	 * @return a new bucket list
+	 */
+	public BucketList<T> refine(Partitioner<T> partitioner)
+	{
+		return new BucketList<T>( //
+				buckets.stream() //
+						.flatMap(bucket -> partitioner.apply(bucket).stream()) //
 						.collect(Collectors.toList()));
 	}
 
--- src/test/java/de/cgarbs/jadupes/filter/BucketListTest.java
+++ src/test/java/de/cgarbs/jadupes/filter/BucketListTest.java
@@ -9,13 +9,13 @@ import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainin
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
-
-import de.cgarbs.jadupes.filter.BucketList;
 
 @SuppressWarnings("javadoc")
 public class BucketListTest
@@ -70,7 +70,7 @@ public class BucketListTest
 	}
 
 	@Test
-	public void refineBucketListBySecondCriteria()
+	public void refineBucketListByClassifier()
 	{
 		// given
 		List<Integer> elements = Arrays.asList(1, 2, 13, 17, 20, 25);
@@ -86,6 +86,26 @@ public class BucketListTest
 						Arrays.asList(2), //
 						Arrays.asList(13, 17, 25), //
 						Arrays.asList(20) //
+				));
+	}
+
+	@Test
+	public void refineBucketListByPartitioner()
+	{
+		// given
+		List<Integer> elements = Arrays.asList(2, 7, 9, 23, 24, 25, 99);
+		BucketList<Integer> groupedByDigitCount = BucketList.create(elements, this::getDigits);
+
+		// when
+		BucketList<Integer> result = groupedByDigitCount.refine(this::createPairs);
+
+		// then
+		assertThat(result.getBuckets().toArray(), //
+				arrayContainingInAnyOrder( //
+						Arrays.asList(2, 7), //
+						Arrays.asList(9), //
+						Arrays.asList(23, 24), //
+						Arrays.asList(25, 99) //
 				));
 	}
 
@@ -116,5 +136,30 @@ public class BucketListTest
 	private boolean evenOdd(Integer i)
 	{
 		return i % 2 == 0;
+	}
+
+	private List<List<Integer>> createPairs(List<Integer> integers)
+	{
+		List<List<Integer>> listOfLists = new ArrayList<>();
+		List<Integer> currentList = null;
+
+		Iterator<Integer> iter = integers.iterator();
+		while (iter.hasNext())
+		{
+			if (currentList != null && currentList.size() > 1)
+			{
+				currentList = null;
+			}
+
+			if (currentList == null)
+			{
+				currentList = new ArrayList<>();
+				listOfLists.add(currentList);
+			}
+
+			currentList.add(iter.next());
+		}
+
+		return listOfLists;
 	}
 }
