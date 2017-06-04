090afbb5ee59d7a5ba3fb81cda540a23e6b3f1f0
Christian Garbs <mitch@cgarbs.de>
2017-06-04 22:52:20 +0200

    add FIXME/TODO

--- src/main/java/de/cgarbs/jadupes/output/OutputAction.java
+++ src/main/java/de/cgarbs/jadupes/output/OutputAction.java
@@ -19,6 +19,8 @@ public interface OutputAction
 	/**
 	 * processes a single group of duplicate files
 	 * 
+	 * FIXME: check for already linked files within the groups!
+	 * 
 	 * @param files
 	 *            the files in the group
 	 */
